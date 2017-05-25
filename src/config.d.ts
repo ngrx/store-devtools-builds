6483f16029d176f40428a28eb94ace2162d51175
Mike Ryan <mike.ryan52@gmail.com>
2017-05-25 03:15:57 +0000

    e21d688 fix(StoreDevtools): Type InjectionToken for AOT compilation

--- src/config.d.ts
+++ src/config.d.ts
@@ -5,7 +5,7 @@ export interface StoreDevtoolsConfig {
     monitor: ActionReducer<any, any>;
     shouldInstrument: Type<boolean> | InjectionToken<boolean>;
 }
-export declare const STORE_DEVTOOLS_CONFIG: InjectionToken<{}>;
-export declare const INITIAL_OPTIONS: InjectionToken<{}>;
+export declare const STORE_DEVTOOLS_CONFIG: InjectionToken<StoreDevtoolsConfig>;
+export declare const INITIAL_OPTIONS: InjectionToken<StoreDevtoolsConfig>;
 export declare const SHOULD_INSTRUMENT: InjectionToken<boolean>;
 export declare type StoreDevtoolsOptions = Partial<StoreDevtoolsConfig> | (() => Partial<StoreDevtoolsConfig>);
