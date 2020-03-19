09d9f78aa292dc861ed08258d0629388b1cd6992
GitHub <noreply@github.com>
2020-03-19 12:16:29 +0000

    b68fa67 feat(schematics): export reducer directly when Ivy is enabled (#2440)

---
+++ schematics-core/utility/json-utilts.d.ts
@@ -0,0 +1,3 @@
+/// <amd-module name="@ngrx/store-devtools/schematics-core/utility/json-utilts" />
+import { JsonAstNode, JsonAstObject } from '@angular-devkit/core';
+export declare function findPropertyInAstObject(node: JsonAstObject, propertyName: string): JsonAstNode | null;
